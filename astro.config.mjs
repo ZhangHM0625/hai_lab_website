818990027bfbb3ba863ba68ff358cb2be555c125
张鹤鸣 <zhangheming@Hemings-MacBook-Pro.local>
2026-02-13 16:25:27 +0800

    feat: initial homepage draft for HAIL lab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 你的 GitHub Pages 顶级域名
+  site: 'https://zhanghm0625.github.io', 
+  // 你的仓库路径，务必保留斜杠
+  base: '/hai_lab_website', 
+});
