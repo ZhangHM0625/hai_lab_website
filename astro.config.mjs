1640a8b705d5b58636ef2e585a50fb2822ba80b9
张鹤鸣 <zhangheming@Hemings-MacBook-Pro.local>
2026-02-13 16:40:08 +0800

    fix: correctly set base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // 你的 GitHub Pages 顶级域名
-  site: 'https://zhanghm0625.github.io', 
+  site:'https://zhanghm0625.github.io', 
   // 你的仓库路径，务必保留斜杠
-  base: '/hai_lab_website', 
+  base:'/hai_lab_website',
 });
